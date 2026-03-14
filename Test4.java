b6b68c1aa2253b95e580502ce9f15b94924be6c4
Sagar <sp120727@gmail.com>
2026-03-14 16:05:37 +0530

    solution

--- Test4.java
+++ Test4.java
@@ -5,7 +5,6 @@ public class Test4 {
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-
                 int top = i;
                 int left = j;
                 int right = size - 1 - j;
